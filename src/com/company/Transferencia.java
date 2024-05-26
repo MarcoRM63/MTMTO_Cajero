9fca0430bf0a4af03355d261e469c314e26caa9d
minji110 <airamguadalupe702@gmail.com>
2024-05-25 17:28:08 -0700

    transferencia modificación

--- src/com/company/Transferencia.java
+++ src/com/company/Transferencia.java
@@ -1,19 +1,33 @@
 package com.company;
 
-public class Transferencia extends Operaciones {
+import java.util.Scanner;
 
+public class Transferencia extends Operaciones implements Movimientos{
+
+    public void retiro(Cuenta c,int tipo, int monto){
+    }
+    //idea de como hacer
+    public void transferir(Cuenta origen, int tipo, Cuenta destino, int monto){
+        Scanner entrada=new Scanner(System.in);
+        tipo=destino.getTipo();
+        System.out.println("Dame el numero de cuenta a la que vas a transferir");
+        int CuentaDestino=entrada.nextInt();
+        destino.setNumeroCuenta(CuentaDestino);
+        System.out.print("Cuanto quieres Transferir: ");
+        Transferencia();
+        System.out.println("Los datos de la transferencia: Cuenta destino "+destino.getNumeroCuenta()+" tipo de cuenta: "+tipo+", de la " +
+                "cuenta origen:"+origen.getNumeroCuenta()+"por el monto de :"+transferencia);
+    }
     @Override
     public void Transacciones() {
-    			System.out.print("Cuanto quieres Transferir: ");
-                Transferencia();
                 if (transferencia <= getSaldo()) {
                     transacciones = getSaldo();
                     setSaldo(transacciones - transferencia);
                     System.out.println("====================================");
                     System.out.println("=================BBVA===============");
                     System.out.println("				Debito				");
-                    System.out.println("Se relalizo una Consulta el 31/10/2023");
-                    System.out.println("A la cuenta "+numero_cuenta);
+                    System.out.println("Se relalizo una Tranferencia el 31/10/2023");
+                    System.out.println("A la cuenta "+ numero_cuenta);
                     System.out.println("transferiste : " + transferencia);
                     System.out.println("Tu saldo actual es: " + getSaldo());
                     System.out.println("====================================");
